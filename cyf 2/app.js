21547351aea6039c6c508e3bf9b0d28d33aaa6f8
Nishka-Kisten <103143568+Nishka-Kisten@users.noreply.github.com>
2022-04-28 01:42:38 +0200

    Add files via upload

---
+++ cyf 2/app.js
@@ -0,0 +1,4 @@
+document.getElementById('image').addEventListener('click', (event) => {
+  event.target.setAttribute("src", 'https://drive.google.com/thumbnail?id=1HlGZm_kvqYgGY48TSmQmkmhb3W5odkyo');
+
+});
